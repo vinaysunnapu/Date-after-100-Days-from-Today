f698659febfda6002ffa26070be6edb39e8c2321
vinaysunnapu <vinaysunnapu@gmail.com>
2022-11-08 05:33:45 +0000

    Date after 100 Days from Today

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+
+const app = express();
+let date = new Date();
+let d = date.getDate();
+let m = date.getMonth();
+let y = date.getFullYear();
+const result = addDays(new Date(y, m, d), 100);
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  let d = date.getDate();
+  let m = date.getMonth();
+  let y = date.getFullYear();
+  const result = addDays(new Date(y, m, d), 100);
+  response.send(
+    `${result.getDate()}/${result.getMonth()}/${result.getFullYear()}`
+  );
+});
+
+module.exports = app;
+
+app.listen(3000);
